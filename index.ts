5acd4cc388404546aa5523600f6ff5b290e0a731
unknown <phamlong123np@gmail.com>
2024-10-28 17:01:10 +0700

    Nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import express, {Express,Request,Response} from "express"
 import * as databse from './config/database'
 import clientRoutes from './routes/client/index.route'
 import dotenv from 'dotenv'
+import { dirname } from "path"
 
 dotenv.config()
 databse.connect() 
 const app:Express= express()
 const port:string|number= process.env.PORT || 3000
+app.use(express.static(`${__dirname}/public`));// có thằng này mới dùng đc các file trong public
 app.set('view engine','pug')
 app.set('views',`${__dirname}/views`)
 clientRoutes(app)
