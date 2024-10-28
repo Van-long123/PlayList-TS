ae3a8355d1ae22c098997f69bd2c2d8d30d02f74
unknown <phamlong123np@gmail.com>
2024-10-28 16:01:44 +0700

    Cài đặt pug

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Starting")
+import express, {Express,Request,Response} from "express"
+const app:Express= express()
+const port:string|number= 3000
+app.set('view engine','pug')
+app.set('views',`${__dirname}/views`)
+app.get('/topics',(req:Request,res:Response)=>{
+    res.render('client/pages/topics/index')
+})
+app.listen(port,()=>{
+    console.log("http://localhost:3000/ "+port)
+})
