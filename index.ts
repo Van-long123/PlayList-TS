1e3bb46c89ad3784280604574c29fff72fc212f5
unknown <phamlong123np@gmail.com>
2024-10-28 16:11:26 +0700

    Kết nối Database

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express, {Express,Request,Response} from "express"
+import * as databse from './config/database'
+
+import dotenv from 'dotenv'
+dotenv.config()
+databse.connect() 
 const app:Express= express()
-const port:string|number= 3000
+const port:string|number= process.env.PORT || 3000
 app.set('view engine','pug')
 app.set('views',`${__dirname}/views`)
 app.get('/topics',(req:Request,res:Response)=>{
